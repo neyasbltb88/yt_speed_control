13a39d2305ac5469daf4725b52277d53d0f6af7e
Neyasbltb <neyasbltb_88@mail.ru>
2022-01-07 18:26:31 +0300

    PlaybackRateView унаследован от EventEmitter. Генерирует события при кликах на стрелки изменения скорости. Добавлены методы setValue и update

--- src/scripts/PlaybackRateView.js
+++ src/scripts/PlaybackRateView.js
@@ -1,20 +1,30 @@
-import { reporter } from '@utils';
 import PlaybackRateBtn from './PlaybackRateBtn';
+import { reporter, EventEmitter } from '@utils';
 
-class PlaybackRateView {
+class PlaybackRateView extends EventEmitter {
     container = null;
     btn = null;
     value = null;
 
-    constructor(value) {
+    constructor(value = 1) {
+        super();
         this.value = value;
     }
 
     onPlus = () => {
-        console.log('onPlus');
+        this.emit('plus');
     };
     onMinus = () => {
-        console.log('onMinus');
+        this.emit('minus');
+    };
+
+    setValue = (value) => {
+        if (value === this.value) return;
+
+        this.value = value;
+        if (!this.container) return;
+
+        this.update();
     };
 
     init(container) {
@@ -27,6 +37,13 @@ class PlaybackRateView {
         let { value, onMinus, onPlus } = this;
         this.btn = PlaybackRateBtn({ value, onMinus, onPlus });
         this.container.insertAdjacentElement('afterbegin', this.btn);
+    }
+    update() {
+        let oldBtn = this.btn;
+        let { value, onMinus, onPlus } = this;
+        this.btn = PlaybackRateBtn({ value, onMinus, onPlus });
+
+        this.container.replaceChild(this.btn, oldBtn);
     }
     destroy() {
         if (!this.btn) {
