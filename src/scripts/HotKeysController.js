4e26005e9881242d6c2494287cc9e68460408b77
neyasbltb88 <neyasbltb_88@mail.ru>
2024-02-17 02:45:46 +0300

    feat: Добавлен HotKeysController для возможности регулировать скорость воспроизведения с помощью сочетания клавиш

---
+++ src/scripts/HotKeysController.js
@@ -0,0 +1,53 @@
+import HotKeys from '@neyasbltb_88/hot-keys-controller';
+import { reporter } from '@utils';
+
+/**
+ * @class
+ * @description Контроллер сочетаний горячих клавиш для управления скоростью воспроизведения
+ */
+export default class HotKeysController {
+    /** Инстанс контроллера горячих клавиш */
+    #hotKeys = null;
+    /** Инстанс основного контроллера скорости воспроизведения */
+    #controller = null;
+    /** Сочетания горячих клавиш, на которые будут срабатывать изменения скорости воспроизведения  */
+    #actions = [
+        {
+            hotKey: 'CTRL+Comma',
+            callback: this.onDecrement.bind(this)
+        },
+        {
+            hotKey: 'CTRL+Period',
+            callback: this.onIncrement.bind(this)
+        }
+    ];
+
+    init(controller) {
+        this.#controller = controller;
+        this.#hotKeys = new HotKeys({ el: 'body', autoEnable: true, tabindex: true });
+
+        this.#hotKeys.setActions(this.#actions);
+    }
+    destroy() {
+        this.#hotKeys?.destroy?.();
+        this.#hotKeys = null;
+        this.#controller = null;
+    }
+
+    onDecrement() {
+        if (!this.#controller) {
+            reporter.error('HotKeysController onDecrement: Отсутствует controller');
+            return;
+        }
+
+        this.#controller.decrementPlaybackRate();
+    }
+    onIncrement() {
+        if (!this.#controller) {
+            reporter.error('HotKeysController onIncrement: Отсутствует controller');
+            return;
+        }
+
+        this.#controller.incrementPlaybackRate();
+    }
+}
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,12 +1,15 @@
 import YouTubePlayerController from './YouTubePlayerController';
 import Controller from './Controller';
+import HotKeysController from './HotKeysController';
 
 let youTubePlayerController = new YouTubePlayerController();
 let controller = new Controller();
+let hotKeysController = new HotKeysController();
 
 const init = () => {
     youTubePlayerController.init();
     controller.init(youTubePlayerController);
+    hotKeysController.init(controller);
 };
 setTimeout(init, 2100);
 
@@ -15,6 +18,7 @@ if (module.hot) {
     module.hot.dispose(() => {
         youTubePlayerController.destroy();
         controller.destroy();
+        hotKeysController.destroy();
     });
 
     module.hot.accept();
