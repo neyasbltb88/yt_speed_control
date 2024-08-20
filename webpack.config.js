6a54657b79d155f320b90013453279242d8f6a5c
neyasbltb88 <neyasbltb_88@mail.ru>
2024-08-21 00:10:19 +0300

    Добавлен плагин бабеля plugin-proposal-nullish-coalescing-operator

--- webpack.config.js
+++ webpack.config.js
@@ -58,6 +58,7 @@ config = {
                         presets: ['@babel/preset-typescript', '@babel/preset-env'],
                         plugins: [
                             '@babel/plugin-proposal-optional-chaining',
+                            '@babel/plugin-proposal-nullish-coalescing-operator',
                             '@babel/plugin-proposal-class-properties',
                             '@babel/plugin-syntax-jsx',
                             ['@babel/plugin-transform-react-jsx', { 'pragma': 'dom' }]
